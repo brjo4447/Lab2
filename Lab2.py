b644bfc8125656ef447e8cf2330d182860be1eab
brjo4447 <35788770+brjo4447@users.noreply.github.com>
2018-01-29 14:09:12 -0700

    Add files via upload

---
+++ Lab2.py
@@ -0,0 +1,49 @@
+"""
+Created on Mon Jan 29 13:20:06 2018
+Lab 2: cinder cone fun
+@author: Brittany
+"""
+
+#Importing stuff
+import numpy as np
+import math#,sys,os,pandas,csv,itertools
+import os
+import csv
+import matplotlib.pyplot as plt
+
+
+'''
+Notes
+Velocity ranges are :
+Angle ranges are :
+Ejecta sizes are : 
+Assume 45 degree angle is fastes w
+    
+'''
+#Creating variables
+g = 9.81 #gravity (m/s2)
+v_min = #min velocity (m/s)
+v_max = #max velocity (m/s)
+D_min = #min ejecta diameter (m)
+D_max = #max ejecta diameter (m)
+
+#Determining grid size
+umax = math.cos(45) * v_max
+wmax = math.sin(45) *vmax
+
+zmax = (wmax**2)/(2*g)
+xmax = (2*umax*wmax)/g 
+
+#np.zeros(shape=(rows,columns))
+#np.zeros(shape=(z,x))
+
+np.zeros(shape=(round(zmax,0),roung(xmax,0))
+
+
+
+#random veolocities
+numpy.random.randint(low, high=None, size=None, dtype='l')
+np.random.randint(1, high=10, size=None, dtype='l')
+
+
+
